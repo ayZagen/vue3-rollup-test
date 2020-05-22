bac7aa42a9002800f663d0179a254ad1b5ac5759
ayazismailhakki <ayazismailhakki@gmail.com>
2020-05-22 15:15:04 +0300

    chore: add comment for @rollup/plugin-typescript configuration

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,10 @@ export default {
     plugins: [
         resolve(),
         typescript(),
+        // with @rollup/plugin-typescript
+        // typescript({
+        //   include: [/\.tsx?$/, /\.vue\?.*?lang=ts/]
+        // }),
         vue(),
     ]
 }
